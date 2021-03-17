fa21cb2009fc77e5150d0fe1c22c1fd4de0b3212
NiallCordes <78307000+NiallCordes@users.noreply.github.com>
2021-03-17 21:21:09 +0800

    Add files via upload
    Added the main method

---
+++ Main.java
@@ -0,0 +1,12 @@
+// Breadth First Search Algorithm
+import java.util.*;
+
+public class Main {
+    //Just a few global variables
+    public static int pathCount;
+    public static int x1;
+    public static int y1;
+    public static int x2;
+    public static int y2;
+
+    public static void main(String[] args) {
