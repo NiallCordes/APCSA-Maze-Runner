673ba05eaee163cded1b3169f98c869d173e233e
NiallCordes <78307000+NiallCordes@users.noreply.github.com>
2021-03-17 22:11:22 +0800

    Add files via upload
    I created a scanner and introduced the normal gameplay method connecting with MazeRunner file. I now will create the Cheat method.

--- Main.java
+++ Main.java
@@ -1,8 +1,8 @@
-// Breadth First Search Algorithm
+// Breadth First Search Algorithm (Part 4 Extension)
 import java.util.*;
 
 public class Main {
-    //Just a few global variables
+    //Setting global variables to use when making the matrix
     public static int pathCount;
     public static int x1;
     public static int y1;
@@ -10,3 +10,16 @@ public class Main {
     public static int y2;
 
     public static void main(String[] args) {
+        Scanner Introduction = new Scanner(System.in); // introducing a scanner
+        System.out.println("Do you want normal gameplay or gameplay with cheats?"); // printing out the question
+        String response = Introduction.nextLine(); //output
+        
+        try{
+            if(response.equalsIgnoreCase("normal")){
+                MazeRunner myRunner = new MazeRunner();
+                Scanner input = new Scanner(System.in); // created a new scanner
+                myRunner.intro();//called on introduction method from MazeRunner
+                myRunner.game(input); // calling on "input" from created scanner
+                System.out.println("You're out alive!");
+            }
+        }
